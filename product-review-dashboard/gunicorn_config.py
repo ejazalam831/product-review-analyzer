64a86e8f7038de3677cd86a5abf14257a7c24d16
ejazalam831 <148932977+ejazalam831@users.noreply.github.com>
2024-12-23 17:05:36 -0800

    Update yaml file

--- product-review-dashboard/gunicorn_config.py
+++ product-review-dashboard/gunicorn_config.py
@@ -1,4 +1,9 @@
 # gunicorn_config.py
-bind = "0.0.0.0:5000"  # Match the port in Dockerfile
-workers = 2
+bind = "0.0.0.0:5000"
+workers = 1  
+worker_class = 'gthread'
+worker_connections = 1000
 timeout = 120
+preload_app = True
+max_requests = 100
+max_requests_jitter = 50
